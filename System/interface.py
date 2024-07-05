6ab77b7b5ee5889df03f291d6ad3c6b7eba57cdd
Alfa06N <nicolasalfag@gmail.com>
2024-07-05 08:26:15 -0400

    Agrega nuevos estilos a los componentes

--- System/interface.py
+++ System/interface.py
@@ -7,7 +7,7 @@ def showLogin(page: ft.Page):
   footer = ft.Row(
     height = 50,
     controls=[
-      ft.Text("© 2024 Kariña System. Todos los derechos reservados.", color="#222222")
+      ft.Text("© 2024 Kariña System. Todos los derechos reservados.", color="#222222", size=16)
     ],
     alignment=ft.MainAxisAlignment.CENTER
   )
